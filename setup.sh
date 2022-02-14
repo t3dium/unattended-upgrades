2e0f5034360696273997279e3ea7d030556f4511
Faraz <83690012+t3dium@users.noreply.github.com>
2022-02-14 10:06:04 +0000

    support for pip, flatpak, snap, pacman, apt, fixes, v1.0

--- setup.sh
+++ setup.sh
@@ -1,17 +1,15 @@
 # The actual script is 1 line, the setup script isnt
 
-#TODO skip errors preventing upgrade from continue, if the user is online ask before running
-
+#TODO skip errors preventing upgrade from continue, if the user is online ask before running, other package managers
+###########################################################################################################
 purple=`tput setaf 5`
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
-
+###########################################################################################################
 add_to_cron(){
-    if [ $how_often == "daily" ]; then
-      thing="@daily apt update; apt full-upgrade -y"
-    else
-      thing="@weekly apt update; apt full-upgrade -y"
+      #concatinating all the cron values
+      thing="${occurence} ${cron_value_distro} ${cron_value_flatpak} ${cron_value_snap} ${cron_value_pip} "
       touch thing
       echo $thing >> thing
       sudo crontab thing
@@ -19,7 +17,7 @@ add_to_cron(){
       echo "Finished, system upgrades have been scheduled without the need for 3k python lines of code."
     fi
 }
-
+###########################################################################################################
 check_root(){
   if [ "$USER" != "root" ]; then
         echo "${red} Permission Denied"
@@ -30,8 +28,55 @@ check_root(){
   fi
   add_to_cron
 }
+###########################################################################################################
 
-echo "$green how frequently would you like system upgrades to occur? $purple [Options] = [daily] [weekly], dont use the []"
-read how_often
+
+###########################################################################################################
+#cron_value_name-here values will all be concatinated later, in the add to cron function
+echo "$red Do not enter [] for the options, just the word inside, also only enable unattended upgrades for package managers you have installed."
+###########################################################################################################
+echo "$green how frequently would you like system upgrades to occur? $purple [Options] = [daily] [weekly]"
+read occurence
+if [ $occurence == "daily" ]; then
+  cron_value_occurence="@daily "
+else
+  cron_value_occurence="@weekly "
+fi
+###########################################################################################################
+echo "$green [Enter] [1] if you're running a debian/ubuntu based distro, or [2] if running arch"
+read distro
+if [ $distro == "1" ]; then
+  cron_value_distro="apt update; apt full-upgrade -y; "
+else
+  cron_value_distro="pacman -Syyu --noconfirm; "
+fi
+###########################################################################################################
+echo "$green Do you want to enable unattended upgrades for flatpaks? [Options] = [y] [n]"
+read flatpak
+if [ $flatpak == "y" ]; then
+  cron_value_flatpak="flatpak update -y --noninteractive; "
+else
+  #making an empty variable to avoid errors during concatination
+  cron_value_flatpak=""
+fi
+###########################################################################################################
+echo "$green Do you want to enable unattended upgrades for snaps? [Options] = [y] [n]"
+read snap
+echo "$red auto-updating snaps may not work as of now, as i cant find the flag to run the command without requiring confirmation"
+if [ $snap == "y" ]; then
+  cron_value_snap="snap refresh; "
+else
+  cron_value_snap=""
+fi
+###########################################################################################################
+echo "$green Do you want to enable unattended upgrades for python pip packages? [Options] = [y] [n]"
+read pip
+if [ $pip == "y" ]; then
+  #using the common pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U seems to break pip somehow so i'm using the pip-review package instead as it works flawlessly
+  pip install pip-review
+  cron_value_pip="pip-review --auto; "
+else
+  cron_value_pip=""
+fi
 #script needs to run as sudo
 check_root
