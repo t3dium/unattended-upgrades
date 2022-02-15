084e125e946b267b9ee031e999dba7e1c44b365d
Faraz <83690012+t3dium@users.noreply.github.com>
2022-02-15 13:07:01 +0000

    Fixed some dumb errors, script actually works now
    Spaces when concatinating resulted in a cron error
    Changed >> crontab -e to | crontab
    @ wasn't showing for some reason.
    Silent flag for docker, no prompt.
    
    Other misc changes

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # The actual script is 1 line, the setup script isnt
 
 #TODO skip errors preventing upgrade from continue, if the user is online ask before running, other package managers
@@ -7,6 +9,7 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 ###########################################################################################################
+
 schedule_cleanup(){
   #Cleanup - these will not be concatinated and will be passed directly to cron. Will need to run after cron file has been created through add_to_cron function
   ###########################################################################################################
@@ -15,18 +18,18 @@ schedule_cleanup(){
   if [ $cleanup_system == "y" ]; then
     #debian/ubuntu
     if [ $distro == "1" ]; then
-      echo "@monthly apt auto-remove -y" >> crontab -e
+      echo "@monthly apt auto-remove -y" >> crontab
     else
       #arch
       #below command pipes excplicit orphans (not optional ones) into pacman's full remove command.
-      echo "pacman -Qtdq | pacman -Rns --noconfirm" >> crontab -e
+      echo "@monthly pacman -Qtdq | pacman -Rns --noconfirm" | crontab
     fi
   fi
   ###########################################################################################################
-  echo "$green Schedule monthly docker cleanup? Old images, unused volumes etc [Options] = [y] [n]"
+  echo "$green Schedule monthly docker cleanup? Old images, unused volumes etc [Options] = [y] [n] $purple"
   read cleanup_docker
   if [ $cleanup_docker == "y" ]; then
-    echo "@monthly docker system prune" >> crontab -e
+    echo "@monthly docker system prune -f" | crontab
   fi
   echo "$purple Finished, system upgrades and/or cleanups have been scheduled without the need for 3k python lines of code."
 }
@@ -34,12 +37,11 @@ schedule_cleanup(){
 add_to_cron(){
       #passing a file to cron (as opposed to simply echo-ing to crontab -e) generates a cron file if none exists.
       #concatinating all the cron values
-      thing="${occurence} ${cron_value_distro} ${cron_value_flatpak} ${cron_value_snap} ${cron_value_pip} "
-      touch thing
-      echo $thing >> thing
-      sudo crontab thing
-      rm thing
-    fi
+      thing="@${occurence} ${cron_value_distro}${cron_value_flatpak}${cron_value_snap}${cron_value_pip}"
+      touch stuff.txt
+      echo $thing >> stuff.txt
+      sudo crontab stuff.txt
+      #rm stuff.txt
     schedule_cleanup
 }
 ###########################################################################################################
@@ -63,12 +65,12 @@ echo "$red Do not enter [] for the options, just the word inside, also only enab
 echo "$green how frequently would you like system upgrades to occur? $purple [Options] = [daily] [weekly]"
 read occurence
 if [ $occurence == "daily" ]; then
-  cron_value_occurence="@daily "
+  cron_value_occurence="daily "
 else
-  cron_value_occurence="@weekly "
+  cron_value_occurence="weekly "
 fi
 ###########################################################################################################
-echo "$green [Enter] [1] if you're running a debian/ubuntu based distro, or [2] if running arch"
+echo "$green [Enter] [1] if you're running a debian/ubuntu based distro, or [2] if running arch $purple"
 read distro
 if [ $distro == "1" ]; then
   cron_value_distro="apt update; apt full-upgrade -y; "
@@ -76,7 +78,7 @@ else
   cron_value_distro="pacman -Syyu --noconfirm; "
 fi
 ###########################################################################################################
-echo "$green Do you want to enable unattended upgrades for flatpaks? [Options] = [y] [n]"
+echo "$green Do you want to enable unattended upgrades for flatpaks? [Options] = [y] [n] $purple"
 read flatpak
 if [ $flatpak == "y" ]; then
   cron_value_flatpak="flatpak update -y --noninteractive; "
@@ -85,7 +87,7 @@ else
   cron_value_flatpak=""
 fi
 ###########################################################################################################
-echo "$green Do you want to enable unattended upgrades for snaps? [Options] = [y] [n]"
+echo "$green Do you want to enable unattended upgrades for snaps? [Options] = [y] [n] $purple"
 read snap
 echo "$red auto-updating snaps may not work as of now, as i cant find the flag to run the command without requiring confirmation"
 if [ $snap == "y" ]; then
@@ -94,7 +96,7 @@ else
   cron_value_snap=""
 fi
 ###########################################################################################################
-echo "$green Do you want to enable unattended upgrades for python pip packages? [Options] = [y] [n]"
+echo "$green Do you want to enable unattended upgrades for python pip packages? [Options] = [y] [n] $purple"
 read pip
 if [ $pip == "y" ]; then
   #using the common pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U seems to break pip somehow so i'm using the pip-review package instead as it works flawlessly
