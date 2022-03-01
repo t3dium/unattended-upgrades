34d751cd45bb96d032b2c79dfbc81d5404341108
Faraz <83690012+t3dium@users.noreply.github.com>
2022-03-01 14:49:19 +0000

    implented TUI (terminal, text gui), misc, ruby support

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+#The actual script is 1 line, the setup script isnt
 
-# The actual script is 1 line, the setup script isnt
-
-#TODO skip errors preventing upgrade from continue, if the user is online ask before running, other package managers
 ###########################################################################################################
 purple=`tput setaf 5`
 red=`tput setaf 1`
@@ -10,40 +8,16 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 ###########################################################################################################
 
-schedule_cleanup(){
-  #Cleanup - these will not be concatinated and will be passed directly to cron. Will need to run after cron file has been created through add_to_cron function
-  ###########################################################################################################
-  echo "$green Schedule monthly removal of unnused dependencies or files not needed anymore (old files etc)? $purple [Options] = [y] [n]"
-  read cleanup_system
-  if [ $cleanup_system == "y" ]; then
-    #debian/ubuntu
-    if [ $distro == "1" ]; then
-      echo "@monthly apt auto-remove -y" >> crontab
-    else
-      #arch
-      #below command pipes excplicit orphans (not optional ones) into pacman's full remove command.
-      echo "@monthly pacman -Qtdq | pacman -Rns --noconfirm" | crontab
-    fi
-  fi
-  ###########################################################################################################
-  echo "$green Schedule monthly docker cleanup? Old images, unused volumes etc [Options] = [y] [n] $purple"
-  read cleanup_docker
-  if [ $cleanup_docker == "y" ]; then
-    echo "@monthly docker system prune -f" | crontab
-  fi
-  echo "$purple Finished, system upgrades and/or cleanups have been scheduled without the need for 3k python lines of code."
-}
-###########################################################################################################
-add_to_cron(){
-      #passing a file to cron (as opposed to simply echo-ing to crontab -e) generates a cron file if none exists.
-      #concatinating all the cron values
-      thing="@${occurence} ${cron_value_distro}${cron_value_flatpak}${cron_value_snap}${cron_value_pip}"
-      touch stuff.txt
-      echo $thing >> stuff.txt
-      sudo crontab stuff.txt
-      #rm stuff.txt
-    schedule_cleanup
-}
+if [ "$USER" != "root" ]; then
+      echo "${red} Permission Denied"
+      echo "${red} Can only be run as root, make sure to run the script with sudo at the start of your command. For e.g, sudo bash linux.sh"
+      exit
+else
+      echo "${green} Welcome"
+fi
+
+#complete rewrite of script in TUI, (whiptail is already installed on most systems)
+echo "$red opening whiptail tui.. if you get an error you might need to install whiptail"
 ###########################################################################################################
 check_root(){
   if [ "$USER" != "root" ]; then
@@ -51,60 +25,76 @@ check_root(){
         echo "${red} Can only be run as root, make sure to run the script with sudo at the start of your command. For e.g, sudo bash linux.sh"
         exit
   else
-        echo "${green} Welcome"
+        echo "${green} continuing..."
   fi
   add_to_cron
 }
 ###########################################################################################################
+if (whiptail --title "check distro" --yesno "Select [yes] if you're using debian/ubuntu, [no] if you're using arch" 8 78); then
+  distro="debian"
+  cron_value_distro="apt update; apt full-upgrade -y; "
+else
+  distro="arch"
+  cron_value_distro="pacman -Syyu --noconfirm; "
 
-
-###########################################################################################################
-#cron_value_name-here values will all be concatinated later, in the add to cron function
-echo "$red Do not enter [] for the options, just the word inside, also only enable unattended upgrades for package managers you have installed."
-###########################################################################################################
-echo "$green how frequently would you like system upgrades to occur? $purple [Options] = [daily] [weekly]"
-read occurence
-if [ $occurence == "daily" ]; then
+fi
+##########################################################################################################
+if (whiptail --title "frequence" --yesno "how frequently would you like system upgrades to occur? [yes] = daily, [no] = weekly" 8 78); then
   cron_value_occurence="daily "
 else
   cron_value_occurence="weekly "
 fi
-###########################################################################################################
-echo "$green [Enter] [1] if you're running a debian/ubuntu based distro, or [2] if running arch $purple"
-read distro
-if [ $distro == "1" ]; then
-  cron_value_distro="apt update; apt full-upgrade -y; "
-else
-  cron_value_distro="pacman -Syyu --noconfirm; "
-fi
-###########################################################################################################
-echo "$green Do you want to enable unattended upgrades for flatpaks? [Options] = [y] [n] $purple"
-read flatpak
-if [ $flatpak == "y" ]; then
+##########################################################################################################
+if (whiptail --title "flatpak updates" --yesno "Do you want to enable unattended upgrades for flatpaks?" 8 78); then
   cron_value_flatpak="flatpak update -y --noninteractive; "
 else
   #making an empty variable to avoid errors during concatination
   cron_value_flatpak=""
 fi
-###########################################################################################################
-echo "$green Do you want to enable unattended upgrades for snaps? [Options] = [y] [n] $purple"
-read snap
-echo "$red auto-updating snaps may not work as of now, as i cant find the flag to run the command without requiring confirmation"
-if [ $snap == "y" ]; then
+##########################################################################################################
+if (whiptail --title "snap updates" --yesno "Do you want to enable unattended upgrades for snaps? - Note: auto-updating snaps may not work as of now, as i cant find the flag to run the command without requiring confirmation" 8 78); then
   cron_value_snap="snap refresh; "
 else
-  cron_value_snap=""
+  ron_value_snap=""
 fi
-###########################################################################################################
-echo "$green Do you want to enable unattended upgrades for python pip packages? [Options] = [y] [n] $purple"
-read pip
-if [ $pip == "y" ]; then
-  #using the common pip3 list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U seems to break pip somehow so i'm using the pip-review package instead as it works flawlessly
+##########################################################################################################
+if (whiptail --title "pip updates" --yesno "Do you want to enable unattended upgrades for python pip packages?" 8 78); then
   pip install pip-review
   cron_value_pip="pip-review --auto; "
 else
   cron_value_pip=""
 fi
 ##########################################################################################################
-#script needs to run as sudo
-check_root
+if [ $distro == "arch" ]; then
+  if (whiptail --title "ruby updates" --yesno "Do you want to enable unattended upgrades for ruby gem packages? - Updating ruby gems this way is currently experimental." 8 78); then
+    cron_value_ruby="gem update; "
+  else
+    cron_value_ruby=""
+    echo "$red Updating ruby packages this way is disabled on debian/ubuntu systems, in order to not risk overwriting debian's ruby package and causing breakage, You should update via apt instead."
+  fi
+fi
+
+##########################################################################################################
+touch stuff.txt
+if (whiptail --title "system cleanup" --yesno "Schedule monthly removal of unnused dependencies or files not needed anymore (old files etc)?" 8 78); then
+  if [ $distro == "debian" ]; then
+    echo "@monthly apt auto-remove -y" >> stuff.txt
+  else
+    #arch
+    #below command pipes excplicit orphans (not optional ones) into pacman's full remove command.
+    echo "@monthly pacman -Qtdq | pacman -Rns --noconfirm" >> stuff.txt
+  fi
+fi
+##########################################################################################################
+if (whiptail --title "docker cleanup" --yesno "Schedule monthly docker cleanup? Old images, unused volumes etc" 8 78); then
+  #debian/ubuntu
+  echo "@monthly docker system prune -f" >> stuff.txt
+fi
+##########################################################################################################
+#passing a file to cron (as opposed to simply echo-ing to crontab -e) generates a cron file if none exists.
+#concatinating all the cron values
+thing="@${cron_value_occurence} ${cron_value_distro}${cron_value_flatpak}${cron_value_snap}${cron_value_pip}${cron_value_ruby}"
+echo $thing >> stuff.txt
+sudo crontab stuff.txt
+rm stuff.txt
+##########################################################################################################
